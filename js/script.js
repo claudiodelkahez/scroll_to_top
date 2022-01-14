e3b1a6fefdd86e8b9f41376e4d5e918511cc8bcc
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-14 19:33:55 -0300

    back to top function ready

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    //console.log(pos);
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //console.log(calcHeight);
+    let calcScrollValue = Math.round((pos * 100) / calcHeight);
+    //console.log(calcScrollValue);
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+}
+
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
