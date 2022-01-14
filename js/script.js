b5fd81f787bfc22d12f524fe54d991dee3cf267f
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-14 19:51:31 -0300

    color progress functionality done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//back to top function
 let calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let progressValue = document.getElementById("progress-value");
@@ -5,14 +6,19 @@ let calcScrollValue = () => {
     //console.log(pos);
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     //console.log(calcHeight);
-    let calcScrollValue = Math.round((pos * 100) / calcHeight);
+    let scrollValue = Math.round((pos * 100) / calcHeight);
     //console.log(calcScrollValue);
     if (pos > 100) {
         scrollProgress.style.display = "grid";
     } else {
         scrollProgress.style.display = "none";
     }
-}
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
 
 
 window.onscroll = calcScrollValue;
